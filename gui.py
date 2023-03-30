d4e9945149938b9880c501821428035235db0be8
Incognito <86202341+alhousen@users.noreply.github.com>
2023-03-30 08:31:59 +0100

    Adds pages to the GUI

--- gui.py
+++ gui.py
@@ -34,3 +34,82 @@ def add_password(self):
     website = self.website_entry.get()
     username = self.username_entry.get()
     password = self.password_entry.get()
+
+    password_data = self.password_manager.add_password(website, username, password)
+    self.table.insert("", tk.END, values=password_data)
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Home Page")
+        label.pack(side="top", fill="x", pady=10)
+        
+        # Create the table
+        table = ttk.Treeview(self, columns=("website", "username", "password"), show="headings")
+        table.pack(padx=650, pady=60)
+        table.heading("website", text="Website")
+        table.heading("username", text="Username")
+        table.heading("password", text="Password")
+        table.pack()
+
+        # Insert some data into the table
+        table.insert("", "end", values=("Facebook.com", "MyUserName", "MyPassword"))
+        table.insert("", "end", values=("Instagram.com", "MyUserName", "MySecondPassword"))
+        table.insert("", "end", values=("LinkedIn.com", "MyUserName", "MyThirdPassword"))
+        
+
+        button1 = tk.Button(self, text="About", command=lambda: controller.show_frame("AboutPage"))
+        button2 = tk.Button(self, text="Contact", command=lambda: controller.show_frame("ContactPage"))
+        button1.pack()
+        button2.pack()
+
+
+
+
+class LoginPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        # Create username label and entry
+        username_label = tk.Label(self, text="Username:")
+        username_label.pack(pady=5)
+        username_entry = tk.Entry(self)
+        username_entry.pack(pady=5)
+
+        # Create password label and entry
+        password_label = tk.Label(self, text="Password:")
+        password_label.pack(pady=5)
+        password_entry = tk.Entry(self, show="*")
+        password_entry.pack(pady=5)
+
+        # Create login button
+        login_button = tk.Button(self, text="Login", command=lambda: self.login(username_entry.get(), password_entry.get()))
+        login_button.pack(pady=5)
+
+    def login(self, username, password):
+        # Add your login logic here
+        print(f"Logging in with username '{username}' and password '{password}'")
+
+class AboutPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="About Page")
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
+        button.pack()
+
+class ContactPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Contact Page")
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Home", command=lambda: controller.show_frame("HomePage"))
+        button.pack()
+
+
