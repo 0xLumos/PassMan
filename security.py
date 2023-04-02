cb9e6df191e0cc50e3863362ed5c169ab904bd68
Incognito <86202341+alhousen@users.noreply.github.com>
2023-04-02 13:03:57 +0100

    Use asymmetric encryption RSA

--- security.py
+++ security.py
@@ -1,47 +1,41 @@
-import hashlib
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
 import secrets
-
-
-
-
-
+import hashlib
+import cryptography
 class EncryptedPasswordManager:
-    
-    def __init__(self, master_password):
-        self.master_password = master_password.encode()
-        self.key = hashlib.pbkdf2_hmac('sha256', self.master_password, b'salt', 100000)
-        print(self.key)
-        self.aesgcm = AESGCM(self.key)
+    def __init__(self):
+
         self.passwords = []
+        self.private_key, self.public_key = self.generate_key_pair()
 
     def add_password(self, website, username, password):
-        website_nonce = secrets.token_bytes(16)  # Generate a random 16-byte (i.e. 128-bit) nonce
+        website_nonce = secrets.token_bytes(16)
         username_nonce = secrets.token_bytes(16)
         password_nonce = secrets.token_bytes(16)
-        website_encrypted = self.aesgcm.encrypt(nonce=website_nonce, data=website.encode(), associated_data=None)
-        username_encrypted = self.aesgcm.encrypt(nonce=username_nonce, data=username.encode(), associated_data=None)
-        password_encrypted = self.aesgcm.encrypt(nonce=password_nonce, data=password.encode(), associated_data=None)
-        encrypted_data = (website_nonce + website_encrypted, username_nonce + username_encrypted, password_nonce + password_encrypted)
+        website_encrypted = self.public_key.encrypt(website.encode(), padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+        username_encrypted = self.public_key.encrypt(username.encode(), padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+        password_encrypted = self.public_key.encrypt(password.encode(), padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+        encrypted_data = (website_encrypted, username_encrypted, password_encrypted)
         self.passwords.append(encrypted_data)
         print("Password added successfully!")
         print(self.passwords)
         return (website, username, password)
-     
+    
+
+    def get_public_key(self,username):
+        return self.public_key
+    
 
     def get_password(self, website, username):
         for website_data, username_data, password_data in self.passwords:
-            website_nonce, website_encrypted = website_data[:16], website_data[16:]
-            username_nonce, username_encrypted = username_data[:16], username_data[16:]
+            website_decrypted = self.private_key.decrypt(website_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+            username_decrypted = self.private_key.decrypt(username_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+            if website_decrypted == website.encode() and username_decrypted == username.encode():
+                password_decrypted = self.private_key.decrypt(password_data, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+                return password_decrypted.decode()
 
-            if website_encrypted == self.aesgcm.encrypt(nonce=website_nonce, data=website.encode(), associated_data=None) and \
-                username_encrypted == self.aesgcm.encrypt(nonce=username_nonce, data=username.encode(), associated_data=None):
-                password_nonce, password_encrypted = password_data[:16], password_data[16:]
-                decrypted_password = self.aesgcm.decrypt(data=password_encrypted, nonce=password_nonce, associated_data=None)
-               
-                return decrypted_password.decode()
-
-
-
-
-
+    def generate_key_pair(self):
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+        return private_key, public_key
