9948c329535cd7110a59f0ed1b6fbd56dce5ddc6
Incognito <86202341+alhousen@users.noreply.github.com>
2023-04-02 13:04:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-from encrypt_decrypt import EncryptedPasswordManager
+from security import EncryptedPasswordManager
 
 from gui import GUI
+
+from storage import MerkleTree
 
 
 if __name__ == "__main__":
     master_password = 'mysecretpassword'
-
+    private_key = None
     # Create an instance of the EncryptedPasswordManager
-    password_manager = EncryptedPasswordManager(master_password)
+    password_manager = EncryptedPasswordManager()
 
     # Add a new password
     website = 'www.example.com'
     username = 'myusername'
     password = 'MyVeryStrongPassword'
     password_manager.add_password(website, username, password)
-
+    
 
 
     # Get a password
     retrieved_password = password_manager.get_password(website, username)
     print(f"Retrieved password: {retrieved_password}")
     app = GUI()
-    app.geometry("1900x700")
-    app.title("PassMan")
+
    
     app.mainloop()
